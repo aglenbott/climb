22b5ac824051485191b9dcc9d1ef636a697e5791
AdamGB <aglenbott@hotmail.co.uk>
2018-11-09 06:51:43 +0000

    updating folder structure

---
+++ src/data_dl.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  6 19:32:28 2018
+
+@author: agb
+"""
+
+#%% change directory
+
+import os
+os.chdir('Users/agb/dev/climb/')
